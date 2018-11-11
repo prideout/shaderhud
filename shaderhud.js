d8ad28572ab7383d80603fb4a98cad3529e1fa00
Philip Rideout <philiprideout@gmail.com>
2018-11-10 16:16:56 -0800

    Implement shader compilation.

--- shaderhud.js
+++ shaderhud.js
@@ -1,3 +1,33 @@
+function create_program(gl, vname, vsource, fname, fsource) {
+    const prog = gl.createProgram();
+
+    const vshader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vshader, vsource);
+    gl.compileShader(vshader);
+    let message = gl.getShaderInfoLog(vshader);
+    if (message.length > 0) {
+        console.warn(`${message}\nwhile compiling [${vname}]`);
+    }
+    gl.attachShader(prog, vshader);
+
+    const fshader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fshader, fsource);
+    gl.compileShader(fshader);
+    message = gl.getShaderInfoLog(fshader);
+    if (message.length > 0) {
+        console.warn(`${message}\nwhile compiling [${fname}]`);
+    }
+    gl.attachShader(prog, fshader);
+
+    gl.linkProgram(prog);
+    if (!gl.getProgramParameter(prog, gl.LINK_STATUS)) {
+        console.error(gl.getProgramInfoLog(prog));
+        return null;
+    }
+    console.info(`Success compiling [${vname}] and [${fname}].`);
+    return prog;
+}
+
 function create_shaderhud(canvas, gl) {
     canvas.insertAdjacentHTML('afterend', `
         <div style="position:absolute;top:0;left:0;right:0" id="shaderhud">
@@ -57,10 +87,19 @@ function create_shaderhud(canvas, gl) {
         applyel.hidden = texteditor.getValue() === shaderentry.applied;
         revertel.hidden = shaderentry.applied === shaderentry.original;
     };
+    const recompileshaders = () => {
+        const programentry = programdb[shaderdb[selectel.value].program.name];
+        const shaderid0 = programentry[0];
+        const shaderid1 = programentry[1];
+        const source0 = shaderdb[shaderid0].applied;
+        const source1 = shaderdb[shaderid1].applied;
+        const progobj = create_program(gl, shaderid0, source0, shaderid1, source1);
+    };
     applyel.addEventListener('click', () => {
         const shaderentry = shaderdb[selectel.value];
         shaderentry.applied = texteditor.getValue();
         updatebuttons();
+        recompileshaders();
     });
     revertel.addEventListener('click', () => {
         const shaderentry = shaderdb[selectel.value];
@@ -87,9 +126,9 @@ function shaderhud(canvas, gl) {
     script.src = 'https://unpkg.com/codemirror@5.41.0/lib/codemirror.js';
     document.head.appendChild(script);
 
-    var fileref = document.createElement('link');
-    fileref.rel = 'stylesheet';
-    fileref.type = 'text/css';
-    fileref.href = 'https://unpkg.com/codemirror@5.41.0/lib/codemirror.css';
-    document.head.appendChild(fileref)
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://unpkg.com/codemirror@5.41.0/lib/codemirror.css';
+    document.head.appendChild(link)
 }
